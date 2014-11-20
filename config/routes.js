e04b992abec3d7a096848ac5b038aa09f84b8165
kmangutov <kmangutov@gmail.com>
2014-11-19 23:21:47 -0600

    some backbonejs frontend working

--- config/routes.js
+++ config/routes.js
@@ -33,7 +33,7 @@ module.exports.routes = {
   ***************************************************************************/
 
   '/': {
-    view: 'homepage'
+    view: 'index'
   },
 
   'post /api/signup': {
