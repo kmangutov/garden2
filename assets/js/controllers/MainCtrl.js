f6a56d8e21e910579e7ced309a50efed18432ac9
Wooyoung Chung <yakle1218@gmail.com>
2014-12-04 20:54:25 -0800

    stored token in session for future authentication

--- assets/js/controllers/MainCtrl.js
+++ assets/js/controllers/MainCtrl.js
@@ -2,26 +2,26 @@
  * Created by R3alFr3e on 12/3/14.
  */
 var mainCtrl = angular.module('MainCtrl',['ngCookies']);
-mainCtrl.controller('MainController', function($scope, $location, $log, $cookieStore, LoginServ){
+mainCtrl.controller('MainController', function($scope, $location, $log, $window, $cookieStore, LoginServ){
     $scope.tagline = "This is SPARTA!";
     $scope.formData = {};
     $scope.isSuccessed = true;
     $scope.islogged = false;
     $scope.result;
-    $scope.user;
-    $scope.user_data = $cookieStore.get('user_data');
 
     $scope.loginUser = function(){
        $log.warn($scope.formData.usr + " vs " + $scope.formData.pw);
        LoginServ.auth($scope.formData.usr, $scope.formData.pw)
           .success(function(data){
-              $log.warn("success to login with " + $scope.formData['usr']);
+              $log.info("success to login with " + $scope.formData['usr']);
               $scope.isSuccessed = true;
               $scope.islogged = true;
-              $scope.result = data;
-              $scope.user = data.user.email;
-              $cookieStore.put('user_data', data);
-              $location.url('/welcome');
+
+              $window.sessionStorage.token = data.token;
+              $window.sessionStorage.userinfo = data.user.email;
+              $log.info(data.user.email);
+
+              $location.url('/volunteer');
           })
           .error(function(data){
               $log.error("error with " + data);
--- assets/js/controllers/VolunteerCtrl.js
+++ assets/js/controllers/VolunteerCtrl.js
@@ -2,11 +2,31 @@
  * Created by R3alFr3e on 12/3/14.
  */
 var volunteerCtrl = angular.module('VolunteerCtrl', []);
-volunteerCtrl.controller('VolunteerController', function($scope){
+volunteerCtrl.controller('VolunteerController', function($scope, $window, $log){
+
     $scope.tagline = "YEEEE GEEE!";
 
-    $scope.username;
+    $scope.userinfo;
+    $scope.token;
+    $scope.isAdmin;
+    $scope.status = false;
+    $scope.statusMessage = '';
 
-    //function to handle add free time
-    //function to handle remove free time
+    $scope.init = function(){
+        if($window.sessionStorage.token &&
+          $window.sessionStorage.userinfo){
+            $scope.userinfo = $window.sessionStorage.userinfo;
+            $scope.token = $window.sessionStorage.token;
+            $scope.isAdmin = $window.sessionStorage.isAdmin;
+            $scope.status = true;
+        }
+        else {
+            $scope.statusMessage = "Please log in D:";
+        }
+    }
+
+    $scope.init();
+
+   //function to handle add free time
+   //function to handle remove free time
 });
