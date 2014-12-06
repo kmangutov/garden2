79f67af0998d9c6ff71fec3ac997250c8e853dae
Wooyoung Chung <yakle1218@gmail.com>
2014-12-05 17:23:32 -0800

    Modified scope of MainController for whole body

--- assets/js/controllers/MainCtrl.js
+++ assets/js/controllers/MainCtrl.js
@@ -2,16 +2,27 @@
  * Created by R3alFr3e on 12/3/14.
  */
 var mainCtrl = angular.module('MainCtrl',['ngCookies']);
-mainCtrl.controller('MainController', function($scope, $location, $log, $window, $cookieStore, LoginServ){
+mainCtrl.controller('MainController', function($scope, $location, $log, $window, $cookieStore, AuthServ){
     $scope.tagline = "This is SPARTA!";
     $scope.formData = {};
     $scope.isSuccessed = true;
     $scope.islogged = false;
     $scope.result;
+    $scope.user;
 
-    $scope.loginUser = function(){
-       $log.warn($scope.formData.usr + " vs " + $scope.formData.pw);
-       LoginServ.auth($scope.formData.usr, $scope.formData.pw)
+    $scope.init = function(){
+      if($window.sessionStorage.token){
+        $scope.islogged = true;
+        $scope.user = $window.sessionStorage.userinfo;
+        $log.info($window.sessionStorage.token);
+        $log.info($window.sessionStorage.userinfo);
+      }
+    };
+    $scope.init();
+
+    $scope.login = function(){
+        $log.warn($scope.formData.usr + " vs " + $scope.formData.pw);
+        AuthServ.auth($scope.formData.usr, $scope.formData.pw)
           .success(function(data){
               $log.info("success to login with " + $scope.formData['usr']);
               $scope.isSuccessed = true;
@@ -19,6 +30,8 @@ mainCtrl.controller('MainController', function($scope, $location, $log, $window,
 
               $window.sessionStorage.token = data.token;
               $window.sessionStorage.userinfo = data.user.email;
+              $scope.user = data.user.email;
+
               $log.info(data.user.email);
 
               $location.url('/volunteer');
@@ -29,7 +42,42 @@ mainCtrl.controller('MainController', function($scope, $location, $log, $window,
               $scope.isSuccessed = false;
               $scope.result = data.error;
           });
-    }
+    };
+
+    $scope.logout = function(){
+      $log.info("User " + $scope.user + " has been logged out");
+      $scope.islogged = false;
+      $scope.isSuccessed = true;
+      delete $window.sessionStorage.token;
+      delete $window.sessionStorage.userinfo;
+      delete $window.sessionStorage.isAdmin;
+
+      $location.url('/');
+    };
+
+    $scope.signup = function() {
+        AuthServ.register($scope.formData.usr, $scope.formData.pw)
+          .success(function(data){
+              $log.info("success to register with " + $scope.formData['usr']);
+              $scope.isSuccessed = true;
+              $scope.islogged = true;
+
+              $window.sessionStorage.token = data.token;
+              $window.sessionStorage.userinfo = data.user.email;
+              $window.sessionStorage.isAdmin = data.user.admin;
+
+              $log.info(data.user.email);
+              $location.url('/volunteer');
+
+          })
+          .error(function(data){
+              $log.error("error with " + data);
+              $scope.islogged = false;
+              $scope.isSuccessed = false;
+              $scope.result = data.error;
+              $log.info(data);
+          });
+    };
 
 });
 
